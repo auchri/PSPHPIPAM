9a8a2a0aab179d19d8713ab8438bee2d622cbabe
Christoph Auer <auer.chrisi.priv@gmail.com>
2018-11-20 16:20:43 +0100

    Add method to add locations

--- functions/Locations.ps1
+++ functions/Locations.ps1
@@ -1,3 +1,21 @@
-function Get-PhpIpamAllLocations{
+function Get-PhpIpamAllLocations {
     return (Invoke-PhpIpamExecute -method get -controller tools -identifiers @('locations')).data
+}
+
+function New-PhpIpamLocation() {
+    param (
+        [string] $Name,
+        [string] $Description,
+        [string] $Address,
+        [double] $Lat,
+        [double] $Long
+    )
+
+    $parameters = @{"name" = $Name; "description" = $Description; "address" = $Address; "lat" = ($Lat -replace ',', '.'); "long" = ($Long -replace ',', '.') }
+
+    $parameters
+
+    $response = Invoke-PhpIpamExecute -method post -controller tools -identifiers @('locations') -params $parameters
+
+    return $response.success
 }
