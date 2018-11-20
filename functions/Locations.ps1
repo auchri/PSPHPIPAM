61d572ba0016a81e5083ee5e18f4de473b743069
Christoph Auer <auer.chrisi.priv@gmail.com>
2018-11-20 16:42:39 +0100

    Implement function to only change some fields of a location

--- functions/Locations.ps1
+++ functions/Locations.ps1
@@ -17,7 +17,45 @@ function New-PhpIpamLocation() {
 
     # Ensure that double values have a dot instead of , - not handled correctly by phpipam :)
     $parameters = @{"name" = $Name; "description" = $Description; "address" = $Address; "lat" = ($Lat -replace ',', '.'); "long" = ($Long -replace ',', '.') }
-    $response = Invoke-PhpIpamExecute -method post -controller tools -identifiers @('locations') -params $parameters
+    return Invoke-PhpIpamExecute -method post -controller tools -identifiers @('locations') -params $parameters
+}
 
-    return $response.success
+function Remove-PhpIpamLocation([int32] $Id) {
+    return Invoke-PhpIpamExecute -method delete -controller tools -identifiers @('locations', $Id)
+}
+
+function Update-PhpIpamLocation() {
+    param (
+        [int32]  $Id,
+        [string] $Name,
+        [string] $Description,
+        [string] $Address,
+        [double] $Lat,
+        [double] $Long
+    )
+    
+    $existingData = Get-PhpIpamLocation -Id $Id
+    $existingData.PsObject.Members.Remove('id') # Remove id
+
+    if($PSBoundParameters.ContainsKey('Name')) {
+        $existingData.name = $Name
+    }
+
+    if($PSBoundParameters.ContainsKey('Description')) {
+        $existingData.description = $Description
+    }
+
+    if($PSBoundParameters.ContainsKey('Address')) {
+        $existingData.address = $Address
+    }
+
+    if($PSBoundParameters.ContainsKey('Lat')) {
+        $existingData.lat = $Lat
+    }
+
+    if($PSBoundParameters.ContainsKey('Long')) {
+        $existingData.long = $Long
+    }
+
+    return Invoke-PhpIpamExecute -method patch -controller tools -identifiers @('locations', $Id) -params $existingData
 }
